8962f9047979c5ddbf67590f553e8fd94259dbd3
Jorgelizarraga94 <jorgelizarraga1994@gmail.com>
2026-05-06 23:01:19 -0300

    Se crea nueva interfaz grafica

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -1,19 +1,20 @@
 package principal;
 
-	
-
 import javax.swing.*;
-
+import org.openstreetmap.gui.jmapviewer.JMapViewer;
 import gui.InterfazDatos;
-	
+import servicio.FiberConnection;
+import servicio.LogicaMapa;
 
 	public class Main extends JFrame
 	{
 		public static void main(String[] args) 
 		{
-			InterfazDatos interfaz = new InterfazDatos();
-			interfaz.setVisible(true);
-			interfaz.setLocationRelativeTo(null);
+			JMapViewer mapa = new JMapViewer();
+			FiberConnection fiberConnection = new FiberConnection();
+			LogicaMapa logicaMapa = new LogicaMapa();
+			InterfazDatos interfazDatos = new InterfazDatos(mapa, fiberConnection);
+			interfazDatos.setVisible(true);
 		}
 	}
 
